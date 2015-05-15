a0d160b8123d8eb2d83427432db486f3cd353a20
Martin Dvorak <martin.dvorak@mindforger.com>
2015-05-15 23:51:30 +0200

    Days and sickdays.

--- src/my-sport-life.py
+++ src/my-sport-life.py
@@ -17,7 +17,6 @@
 import yaml
 
 
-
 class MySportLife:
    version=1.0
 
@@ -36,17 +35,43 @@ class TrainingPhase:
 # drives Python datastructures and code here only performs
 # traversal and analytics of these structures.
 class TrainingLog:
-    
+
+    def __init__(self, logFileName):
+        self.logFileName = logFileName
+
     # Load all data files declared in config.yaml
     def load(self):
-        print 'TBD'
+        stream = open(self.logFileName, 'r')
+        self.data = yaml.load(stream, Loader=yaml.CLoader)
+        return self.data
 
-    def loadAndDump(self):
-        stream = open("config.yaml", 'r')
-        data = yaml.load(stream, Loader=yaml.CLoader)
-        print yaml.dump(data)
 
 class Report:
+    daysWorthIt = set([])
+    sickDays = set([])
+    activityTypes = set([])
+    # TODO this must be dictionary and activit has dict with 'km' where its evaluated
+    totalConcept2Phases = 0
+    totalRunningPhases = 0
+    totalConcept2Km = 0
+    totalRunningKm = 0
+
+    def __init__(self, data):
+        self.data = data
+
+    def calculate(self):
+        print 'Calculating year: {}'.format(self.data.get('year'))
+        for phase in self.data.get('log'):
+            if phase.get('activity') == 'sick':
+                self.sickDays.add(phase.get('date'))
+            else:
+                self.daysWorthIt.add(phase.get('date'))
+            if phase.get('activity') not in self.activityTypes:
+                self.activityTypes.add(phase.get('activity'))
+            if phase.get('activity') == 'Concept2':
+                self.totalConcept2Phases+=1
+                #self.totalConcept2Km += phase.get('distance')
+            print '  {}'.format(phase.get('date'))
 
     # For every piece of equipment evaluate how much 1km cost
     def equipmentCostPerKm(self):
@@ -71,10 +96,25 @@ class Report:
     def meTotalUnitsForEachActivity(self):
         print 'TBD'
 
+    def printStatistics(self):
+        # TODO make this single string
+        print '\nReport:'
+        print '  Active days: {}'.format(len(self.daysWorthIt))
+        print '  Sick days: {}'.format(len(self.sickDays))
+        print '  Phases: {}'.format(len(self.data.get('log'))-len(self.sickDays))
+        print '  Running phases: {}'.format(self.totalRunningPhases)
+        print '  Running km: {}'.format(self.totalRunningKm)
+        print '  Concept2 phases: {}'.format(self.totalConcept2Phases)
+        print '  Concept2 km: {}'.format(self.totalConcept2Km)
+        print '\n'
+
 
 # main()
 
-log = TrainingLog()
-log.loadAndDump()
+#log = TrainingLog("config.yaml")
+log = TrainingLog('../examples/20-years/2015.yaml')
+report = Report(log.load())
+report.calculate()
+report.printStatistics()
 
 # eof
