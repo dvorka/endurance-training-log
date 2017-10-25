84c44d86ac7086278ca7242a67c7c38c3cc637f3
Martin Dvorak <martin.dvorak@mindforger.com>
2017-10-25 23:17:52 +0200

    Ranaming project.

---
+++ bin/endurance-training-log.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/dvorka/p/endurance-training-log/github/endurance-training-log/src && ./endurance-training-log.py
+
+# eof
--- bin/mysportlife.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/dvorka/p/my-sport-life/github/my-sport-life/src && ./my_sport_life.py
-
-# eof
